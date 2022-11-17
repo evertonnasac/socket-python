e197a57ed006c914db248386839aa4901a85ddae
Everton Sacramento <nazare.everton@gmail.com>
2022-11-17 00:47:42 -0300

    Implementando função de busca valor de vendas por periodo

--- gerente.py
+++ gerente.py
@@ -22,17 +22,57 @@ cliente.connect((HOST,PORT))
 # Realiza a troca de mensagens com o servidor
 def sendMessage(mensagem):
 
-    mensagemEncoded = str(mensagem)
-    cliente.sendall((mensagemEncoded.encode("utf-8")))
+    mensagemStr = str(mensagem)
+    cliente.sendall((mensagemStr.encode("utf-8")))
 
     resposta = cliente.recv(1024)
     
     return resposta.decode("utf-8")
 
+
+
+def getData():
+	controle = True
+
+	while controle:
+		print("Siga as orientações abaixo para informar a data corretamente")
+		
+		try:
+
+			print("Digite o dia na data a ser pesquisada ")
+			dia = int((input()))
+
+			print("Digite o numero do mes correspondente a data a ser pesquisada:\n" +
+
+				"1 -  JANEIRO\n" +
+				"2 -  FEVEREIRO\n" +
+				"3 -  MARÇO\n" +
+				"4 -  ABRIL\n" +
+				"5 -  MAIO\n" +
+				"6 -  JUNHO\n" +
+				"7 -  JULHO\n" +
+				"8 -  AGOSTO\n" +
+				"9 -  SETEMBRO\n" +
+				"10 - OUTUBRO\n" +
+				"11 - NOVEMBRO\n" +
+				"12 - DEZEMBRO\n" 
+			)
+			mes = int(input())
+
+			print("Digite o ano por extenso (com 4 dígitos) na data a ser pesauisada. Exemplo: 2022 ou 1998")
+			ano = int(input())
+			
+			data = datetime.date(ano, mes, dia)
+			controle = False
+			return data
+
+		except: 
+			print("Por favor, preencha os campos de forma correta")
+
 # Função que retorna o total de vendas de um vendedor escolhido pelo usuário
 def getTotalVendedor(opcao):    
     print("Vendedores cadastrados:\nAna\nJoao\nJose\nMaria")
-    print("Digite o nome do vendedor")
+    print("Digite o nome do vendedor entre os listados acima")
     nome = input()
 
     # Cria um dicionário com os dados e passa para a função que troca menssagens com o servidor
@@ -82,10 +122,21 @@ def getMelhorVendedor(opcao):
             lista[x] = eval(lista[x])
             print(f"Nome: {lista[x]['nome'].title()} --- Valor: {lista[x]['vendas']}")
 
-        
-        
+def getTotalPeriodo(opcao):
 
-    
+    print("Informe a data INICIAL do período a ser pesquisado")
+    dataInicial = getData()
+    print("Informe a data FINAL do período a ser pesquisado")
+    dataFinal = getData()
+
+    print("END")
+
+    mensagem = {"operacao": CODIGO_OPERACAO, "opcao": opcao, "inicial": dataInicial, "final": dataFinal}
+    retorno =  sendMessage(mensagem)
+    print(retorno)
+
+
+
 
 def getTotalVendasUmaLoja(opcao):
     print("Digite o código da loja")
@@ -96,9 +147,11 @@ def getTotalVendasUmaLoja(opcao):
     mensagem = {"operacao": CODIGO_OPERACAO, "opcao": opcao, "loja": loja }
     retorno = eval(sendMessage(mensagem))
 
-    loja = retorno["loja"]
-    total = retorno["total"]
-    print(f"Loja: {loja} --- Total de vendas: {total} ")
+    if retorno["type"] == "string":
+        print(retorno["message"])
+        return
+
+    print(f"Loja: {retorno['loja']} --- Total de vendas: {retorno['total']} ")
 
 
 def getMelhorLoja(opcao):
@@ -109,9 +162,31 @@ def getMelhorLoja(opcao):
         print(resposta["message"])
         return 
 
-    
+    # A chave "type" com valor "dict" signifia que só retornou uma loja
+    elif resposta["type"] == "dict":
+
+        # Nesse caso, a chave "valor" é uma string com o seguinte modelo: {"loja": "xxx", "total":"yyy"}
+        loja = eval(resposta["valor"])
+        
+        print(f"Nome: {loja['loja'].title()} --- Total em vendas: {loja['total']}")
+        
+    # a chave "type" com valor "list" revela que foram retornados mais de uma loja com valores de vendas iguais
+    elif resposta["type"] == "list":
+
+        # Nesse caso, a chave "valor" é uma string com o modelo igual a condição anterior, com mais ocorrências, separadas por "*"
+        # Exemplo: {"loja": "xxx", "total":"yyy"}*{"loja": "xxx", "total":"yyy"}....
+        lista = resposta["valor"]
+
+        # Os dados são convertidos em uma lista e em seguida, cada item são convertidos em dicionario
+        lista = list(lista.split("*"))
+
+        print("As lojas com maior valor em vendas são:")
+        for x in range(len(lista)):
+            lista[x] = eval(lista[x])
+            print(f"Nome: {lista[x]['loja'].title()} --- Valor: {lista[x]['total']}")
 
 
+#...........................Menu de opções.......................
 
 print("DIGITE QUALQUER LETRA PARA INICIAR A OPERAÇÃO OU DIGITE O NUMERO 0 (ZERO) PARA ENCERRAR")
 entrada = input()
@@ -136,10 +211,10 @@ while entrada != "0":
         getTotalVendasUmaLoja(opcao)
 
     elif opcao == "4":
-        tese = 0
+        getTotalPeriodo(opcao)
     
     elif opcao == "5":
-        getMelhorVendedor(opcao)
+        getMelhorLoja(opcao)
 
     print("DIGITE QUALQUER LETRA PARA INICIAR A OPERAÇÃO OU DIGITE O NUMERO 0 (ZERO) PARA ENCERRAR")
     entrada = input()
--- servidor.py
+++ servidor.py
@@ -12,9 +12,17 @@ vendedores = [
 ]
 
 
-loja_1 = []
+loja_1 = [
+    {"valor": 200.00, "data": datetime.date(2022,10,20)},
+    {"valor": 100.00, "data": datetime.date(2022,7,20)},
+    {"valor": 400.00, "data": datetime.date(2022,9,20)}
+]
 
-loja_2 = []
+loja_2 = [
+    {"valor": 300.00, "data": datetime.date(2022,11,20)},
+    {"valor": 50.00, "data": datetime.date(2022,6,20)},
+    {"valor": 400.00, "data": datetime.date(2022,5,20)}
+]
 
 
 
@@ -41,20 +49,53 @@ def registrarVenda(venda):
     
     return "ERRO: Operção não realizada"
 
-    
 		
 # Retorna a idenficação da loja que mais vendeu
 def getMelhorLoja ():
     total = []
 
     if len(loja_1) == 0 and len(loja_2) == 0 :
-        return {"type":"string", "message": "Nenhuma loja registrou venda"}
+        return {"type":"string", "message": "Nenhuma loja tem venda registrada"}
 
-    total.append(calculaTotalLoja(loja_1, "Loja_1"))
-    total.append(calculaTotalLoja(loja_2, "Loja_2"))
+    # Trazendo o somatório do valor de vendas de cada lista de lojas
+    loja1 = calculaTotalLoja(loja_1, "Loja_1")
+    loja2 = calculaTotalLoja(loja_2, "Loja_2")
 
-    ordem = sorted(total, key = lambda row: row["valor"], reverse=1)
-    return ordem[0]
+    # Filtrando somente  os dados necessários e adicionando na lista total
+    loja1 = {"loja": loja1["loja"], "total": loja1["total"]}
+    total.append(loja1)
+
+    loja2 = {"loja": loja2["loja"], "total": loja2["total"]}
+    total.append(loja2)
+
+    # Iniciando a verificação de qual loja tem o maior valor total
+    valor = 0.0
+    melhorLoja = []
+
+    for item in total :
+
+        if item["total"] <= 0.0:
+            continue
+
+        if item["total"] > valor:
+            valor = item["total"]
+            melhorLoja.clear()
+            melhorLoja.append(str(item))
+        
+        elif item["total"] == valor:
+            melhorLoja.append(str(item))
+
+    if len(melhorLoja) == 0:
+        return {"type": "string", "message" :"Nenhuma loja tem venda registrada"}
+
+    elif len(melhorLoja) == 1:
+        return {"type": "dict", "valor": melhorLoja[0]}
+
+    return {"type": "list", "valor": "*".join(melhorLoja)}
+
+
+    #ordem = sorted(total, key = lambda row: row["valor"], reverse=1)
+    #return ordem[0]
 
 
 # Calcula o total de vendas de uma loja
@@ -67,12 +108,53 @@ def calculaTotalLoja (array, descricao):
     return {"type":"dict", "loja": descricao, "total": total}
 
 
-# Retorna o (os) vendor com maior numero de vendas
+#Calcula o total de vendas da rede por periodo
+def calculaPeriodo(inicial, final):
+
+    #Cria uma lista com todos os registros das  listas correspondente a cada loja
+    totalVendas = loja_1 + loja_2
+    
+    # Ordena os registros de venda por maior data
+    totalVendas = sorted(totalVendas, key = lambda row: row["data"])
+
+    indiceInical =-1
+    indiceFinal = len(totalVendas)
+
+    # busca o indice inical para percorrer a lista "totalVendas" 
+    for venda in totalVendas:
+        if venda["data"] >= inicial:
+            indiceInical = totalVendas.index(venda)
+            break
+
+    if indiceInical < 0:
+        return "Não existe vendas realizadas a partir da data inicial informada"
+
+    # Busca o indice final para percorrer a lista "totalVendas"    
+    for venda in range(indiceInical, len(totalVendas)):
+        if totalVendas[venda]["data"] > final:
+            indiceFinal = venda
+            break
+
+    # Realiza a soma do valor das vendas na lista "totalVendas" 
+    #  a partir do indice inicial e final encontrados
+    valorTotal = 0.0
+    for venda in range(indiceInical, indiceFinal):
+        valorTotal += totalVendas[venda]["valor"]
+    
+    print(valorTotal)
+
+    return f"O Valor total de vendas no período de {inicial} a {final} é de {valorTotal}"
+
+
+# Retorna o (os) vendor(es) com maior numero de vendas
 def getMelhorVendedor ():
     vendedor = []
     valor = 0.0
 
     for func in vendedores:
+        if func["vendas"] <= 0.0:
+            continue
+        
         if func["vendas"] > valor:
             valor = func["vendas"]
             vendedor.clear()
@@ -91,7 +173,7 @@ def getMelhorVendedor ():
 
 
 
-# Retorna o valor total de vendas do vendor requerido
+# Retorna o valor total de vendas do vendedor requerido
 def getTotalVendedor(vendedor):
     for func in vendedores:
         if func["nome"] == vendedor:
@@ -154,10 +236,10 @@ while (True):
             elif dados["loja"] == "2":
                 retorno = calculaTotalLoja(loja_2, "Loja 2")
             else:
-                retorno = " ERROR: Loja não encontrada"
+                retorno = {"type": "string", "message" : "ERROR: Loja não encontrada"}
         
         elif dados["opcao"] == "4":
-            retorno = "Error 404 not found"
+            retorno = calculaPeriodo(dados["inicial"], dados["final"])
 
         elif dados["opcao"] == "5":
             retorno = getMelhorLoja()
